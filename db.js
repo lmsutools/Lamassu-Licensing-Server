c19f9fc7c6738540df0365801937732ba9d636cf
lmsutools <lcomunicamos3@gmail.com>
2023-04-11 10:50:50 -0500

    as

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mariadb = require("mariadb"),
     pool = mariadb.createPool({
         host: "localhost",
+        port: 3080,
         user: "admin_license_server",
         password: "admin123",
         database: "licenses_db",
